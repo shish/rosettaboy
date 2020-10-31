8d4aafe8f4f3fee2ec16e970cd1dd0fc710ef37b
Shish <shish@shishnet.org>
2020-10-31 02:53:33 +0000

    also add the new file

---
+++ py/buttons.py
@@ -0,0 +1,73 @@
+import pygame
+
+class Buttons:
+    def __init__(self):
+        self.cycle = 0
+        self.up = False
+        self.down = False
+        self.left = False
+        self.right = False
+        self.a = False
+        self.b = False
+        self.start = False
+        self.select = False
+
+    def tick(self) -> bool:
+        lx = self.cycle % 114
+        ly = (self.cycle / 114) % 154
+        self.cycle += 1
+
+        if not self.update_buttons():
+            return False
+
+        if lx == 20 and ly == 0:
+            return self.handle_inputs()
+        else:
+            return True
+
+
+    def update_buttons(self) -> bool:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.locals.K_ESCAPE:
+                    return False
+                if event.key == pygame.locals.K_Z:
+                    self.b = True
+                if event.key == pygame.locals.K_X:
+                    self.a = True
+                if event.key == pygame.locals.K_RETURN:
+                    self.start = True
+                if event.key == pygame.locals.K_SPACE:
+                    self.select = True
+                if event.key == pygame.locals.K_UP:
+                    self.up = True
+                if event.key == pygame.locals.K_DOWN:
+                    self.down = True
+                if event.key == pygame.locals.K_LEFT:
+                    self.left = True
+                if event.key == pygame.locals.K_RIGHT:
+                    self.right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.locals.K_Z:
+                    self.b = False
+                if event.key == pygame.locals.K_X:
+                    self.a = False
+                if event.key == pygame.locals.K_RETURN:
+                    self.start = False
+                if event.key == pygame.locals.K_SPACE:
+                    self.select = False
+                if event.key == pygame.locals.K_UP:
+                    self.up = False
+                if event.key == pygame.locals.K_DOWN:
+                    self.down = False
+                if event.key == pygame.locals.K_LEFT:
+                    self.left = False
+                if event.key == pygame.locals.K_RIGHT:
+                    self.right = False
+        return True
+
+    def handle_inputs(self) -> bool:
+        # TODO: implement this
+        return True
