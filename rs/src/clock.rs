03faf8e0099c2eafdb9ad024115c1a1cc789083c
Shish <shish@shishnet.org>
2020-11-05 23:28:00 +0000

    show busy time

--- rs/src/clock.rs
+++ rs/src/clock.rs
@@ -5,6 +5,7 @@ pub struct Clock {
     last_frame_start: SystemTime,
     last_report: SystemTime,
     time_per_frame: Duration,
+    sleep_duration: Duration,
     cycle: u32,
     frame: u32,
     profile: u32,
@@ -18,12 +19,14 @@ impl Clock {
         let last_frame_start = SystemTime::now();
         let time_per_frame = Duration::from_millis((1000.0 / 60.0) as u64);
         let last_report = SystemTime::now();
+        let sleep_duration = Duration::new(0, 0);
 
         Clock {
             start,
             last_frame_start,
             last_report,
             time_per_frame,
+            sleep_duration,
             cycle: 0,
             frame: 0,
             profile,
@@ -42,7 +45,9 @@ impl Clock {
                 .duration_since(self.last_frame_start)
                 .expect("time");
             if !self.turbo && time_spent < self.time_per_frame {
-                ::std::thread::sleep(self.time_per_frame - time_spent);
+                let sleep_time = self.time_per_frame - time_spent;
+                self.sleep_duration += sleep_time;
+                ::std::thread::sleep(sleep_time);
             }
             self.last_frame_start = SystemTime::now();
 
@@ -51,7 +56,8 @@ impl Clock {
                 let t = SystemTime::now();
                 let fps =
                     60000.0 / (t.duration_since(self.last_report).unwrap().as_millis()) as f32;
-                println!("{:.1}fps", fps);
+                println!("{:.1}fps, {:.1}% busy", fps, (1.0-self.sleep_duration.as_secs_f32())*100.0);
+                self.sleep_duration = Duration::new(0, 0);
                 self.last_report = t;
             }
 
