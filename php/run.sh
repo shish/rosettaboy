4fbf57d69867e1c9e03c1e6970d4ccd46a142285
Shish <shish@shishnet.org>
2021-12-10 22:41:50 +0000

    php github actions, fixes, #20

--- php/run.sh
+++ php/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 # opcache in 8.1 gives a nice speedup (25s to 10s)
-/usr/local/Cellar/php/8.1.0/bin/php -dopcache.enable_cli=1 -dopcache.jit_buffer_size=100M src/main.php $*
+if [ -f /usr/local/Cellar/php/8.1.0/bin/php ] ; then
+    /usr/local/Cellar/php/8.1.0/bin/php \
+        -dopcache.enable_cli=1 -dopcache.jit_buffer_size=100M \
+        src/main.php $*
+else
+    php src/main.php $*
+fi
--- php/src/main.php
+++ php/src/main.php
@@ -1,7 +1,8 @@
 <?php
 
 // FFS PHP
-function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+function exception_error_handler($errno, $errstr, $errfile, $errline)
+{
     throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
 }
 set_error_handler("exception_error_handler");
