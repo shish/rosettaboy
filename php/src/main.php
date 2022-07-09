30ad5fdce7c42bb06d1202d8a7f10bc064a01fc9
Shish <shish@shishnet.org>
2022-07-09 19:26:29 +0100

    long opts for php

--- php/src/main.php
+++ php/src/main.php
@@ -16,19 +16,34 @@ require "clock.php";
 require "buttons.php";
 require "ram.php";
 
-$rest_index = null;
-$opts = getopt("cgrHStp:", ["rom"], $rest_index);
-$pos_args = array_slice($argv, $rest_index);
-//var_dump($opts);
-//var_dump($pos_args);
+function parse_args($argv)
+{
+    $rest_index = null;
+    $opts = getopt("cgraHStp:", ["debug-cpu", "debug-gpu", "debug-ram", "debug-apu", "headless", "silent", "turbo", "profile:"], $rest_index);
+    $pos_args = array_slice($argv, $rest_index);
+    //var_dump($opts);
+    //var_dump($pos_args);
+    return [
+        "debug-cpu" => array_key_exists('c', $opts) || array_key_exists('debug-cpu', $opts),
+        "debug-gpu" => array_key_exists('g', $opts) || array_key_exists('debug-gpu', $opts),
+        "debug-ram" => array_key_exists('r', $opts) || array_key_exists('debug-ram', $opts),
+        "debug-apu" => array_key_exists('a', $opts) || array_key_exists('debug-apu', $opts),
+        "headless" => array_key_exists('H', $opts) || array_key_exists('headless', $opts),
+        "silent" => array_key_exists('S', $opts) || array_key_exists('silent', $opts),
+        "turbo" => array_key_exists('t', $opts) || array_key_exists('turbo', $opts),
+        "profile" => array_key_exists('p', $opts) ? $opts['p'] : (array_key_exists('profile', $opts) ? $opts['profile'] : 0),
+        "rom" => $pos_args[0],
+    ];
+}
 
-$cart = new Cart($pos_args[0]);
-$ram = new RAM($cart, array_key_exists('r', $opts));
-$cpu = new CPU($ram, array_key_exists('c', $opts));
-$gpu = new GPU($cpu, array_key_exists('g', $opts), array_key_exists('H', $opts));
-$apu = new APU(array_key_exists('S', $opts), array_key_exists('a', $opts));
-$buttons = new Buttons($cpu, array_key_exists("H", $opts));
-$clock = new Clock($buttons, array_key_exists('p', $opts) ? $opts['p'] : 0, array_key_exists('t', $opts));
+$args = parse_args($argv);
+$cart = new Cart($args['rom']);
+$ram = new RAM($cart, $args['debug-ram']);
+$cpu = new CPU($ram, $args['debug-cpu']);
+$gpu = new GPU($cpu, $args['debug-gpu'], $args['headless']);
+$apu = new APU($args['silent'], $args['debug-apu']);
+$buttons = new Buttons($cpu, $args['headless']);
+$clock = new Clock($buttons, $args['profile'], $args['turbo']);
 
 while (true) {
     $cpu->tick();
