7b860662186fc0c4e6abde9115a88ee71a635dbf
Shish <shish@shishnet.org>
2022-07-30 16:45:04 -0700

    [nim] add SDL stubs

--- nim/rosettaboy.nimble
+++ nim/rosettaboy.nimble
@@ -11,3 +11,4 @@ bin           = @["rosettaboy"]
 # Dependencies
 
 requires "nim >= 1.6.6"
+requires "sdl2"
--- nim/src/gpu.nim
+++ nim/src/gpu.nim
@@ -1,3 +1,5 @@
+import sdl2
+
 type
     GPU* = object
         cart_name: string
@@ -15,6 +17,32 @@ type
         ]#
 
 proc create*(cart_name: string, headless: bool, debug: bool): GPU =
+    if not headless:
+        discard sdl2.init(INIT_EVERYTHING)
+        var
+            window: WindowPtr
+            render: RendererPtr
+
+        window = createWindow("SDL Skeleton", 100, 100, 640,480, SDL_WINDOW_SHOWN)
+        render = createRenderer(window, -1, Renderer_Accelerated or Renderer_PresentVsync or Renderer_TargetTexture)
+
+        var
+            evt = sdl2.defaultEvent
+            runGame = true
+
+        while runGame:
+            while pollEvent(evt):
+                if evt.kind == QuitEvent:
+                    runGame = false
+                    break
+
+            render.setDrawColor 0,0,0,255
+            render.clear
+            render.present
+
+        destroy render
+        destroy window
+
     return GPU(
       cart_name: cart_name,
       headless: headless,
