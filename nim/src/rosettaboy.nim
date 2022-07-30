064c6aa838a39ba1aba7af245ac96dfe1bc143c3
Shish <shish@shishnet.org>
2022-07-30 15:22:00 -0700

    [nim] implement a bunch of errors

--- nim/src/rosettaboy.nim
+++ nim/src/rosettaboy.nim
@@ -8,6 +8,15 @@ when isMainModule:
         const args_obj = args.parse_args()
         var gameboy_obj = gameboy.create(args_obj)
         gameboy_obj.run()
-    except errors.Timeout as e:
+    except errors.UnitTestFailed as e:
+        echo e.msg
+        system.quit(2)
+    except errors.ControlledExit as e:
         echo e.msg
         system.quit(0)
+    except errors.GameException as e:
+        echo e.msg
+        system.quit(3)
+    except errors.UserException as e:
+        echo e.msg
+        system.quit(4)
