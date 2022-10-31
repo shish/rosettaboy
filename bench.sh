d05f2ce7afaff5de00c40ccc74defb2693af6fc3
Shish <shish@shishnet.org>
2022-10-31 21:31:14 +0000

    more complicated bench script which allows for compiler variants

--- bench.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for run in */run.sh ; do
-    n=$(dirname $run)
-    out=$(cd $n && ./run.sh --profile 600 --silent --headless --turbo ../test_roms/games/opus5.gb 2>&1 | grep frames)
-    printf "%5s %s\n" "$n:" "$out"
-done
