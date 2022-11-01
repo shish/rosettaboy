22ed81edba00ec741b63a92bb871709f1e02d5c0
Shish <shish@shishnet.org>
2022-11-01 00:11:30 +0000

    format

--- bench.py
+++ bench.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+Usage: ./bench.py zig rs cpp
+
+Will find any run*.sh scripts in the named directories
+(eg run.sh, run_pgo.sh, run_pypy.sh) and run them with
+a standard set of args.
+"""
 from glob import glob
 import subprocess
 import os
@@ -9,21 +16,30 @@ import sys
 for lang_runner in glob("*/run*.sh"):
     lang = os.path.dirname(lang_runner)
     runner = os.path.basename(lang_runner)
-    
+
     if sys.argv[1:] and lang not in sys.argv[1:]:
         continue
-    
+
     sub = "release"
     if match := re.match("run_(.*).sh", runner):
         sub = match.group(1)
-    #print(f"{lang} / {runner} / {sub}")
     proc = subprocess.run(
-        [f"./{runner}", "--profile", "600", "--silent", "--headless", "--turbo", "../test_roms/games/opus5.gb"],
-        cwd=lang, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+        [
+            f"./{runner}",
+            "--profile",
+            "600",
+            "--silent",
+            "--headless",
+            "--turbo",
+            "../test_roms/games/opus5.gb",
+        ],
+        cwd=lang,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
     )
     frames = ""
     for line in proc.stdout.split("\n"):
         if "frames" in line:
             frames = line
     print(f"{lang:>5s} / {sub:7s}: {frames}")
-    #printf "%5s %s\n" "$n:" "$out"
