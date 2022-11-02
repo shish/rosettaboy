fec428d5034db7525be88ac875611a09a4c95052
Shish <shish@shishnet.org>
2022-11-02 15:15:33 +0000

    [utils] --parallel option for bench.py, to give bad results quickly

--- utils/bench.py
+++ utils/bench.py
@@ -13,6 +13,7 @@ import os
 import re
 import sys
 import argparse
+from multiprocessing.pool import ThreadPool
 
 TEST_ROM_URL = "https://github.com/sjl/cl-gameboy/blob/master/roms/opus5.gb?raw=true"
 TEST_ROM = "/tmp/opus5.gb"
@@ -26,6 +27,34 @@ if not os.path.exists(TEST_ROM):
     )
 
 
+def test(lang: str, runner: str, sub: str, frames: int) -> bool:
+    proc = subprocess.run(
+        [
+            f"./{runner}",
+            "--profile",
+            str(frames),
+            "--silent",
+            "--headless",
+            "--turbo",
+            TEST_ROM,
+        ],
+        cwd=lang,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
+    if proc.returncode != 0:
+        print(f"{lang:>5s} / {sub:7s}: Failed\n{proc.stdout}")
+        return False
+    else:
+        frames = ""
+        for line in proc.stdout.split("\n"):
+            if "frames" in line:
+                frames = line
+        print(f"{lang:>5s} / {sub:7s}: {frames}")
+        return True
+
+
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -35,13 +64,18 @@ def main() -> int:
         help="Only run the default run.sh, not variants",
     )
     parser.add_argument(
+        "--parallel",
+        default=False,
+        action="store_true",
+        help="Run all tests in parallel (gives inaccurate results, quickly)",
+    )
+    parser.add_argument(
         "--frames", type=int, default=600, help="Run for this many frames"
     )
     parser.add_argument("langs", default=[], nargs="*")
     args = parser.parse_args()
 
-    all_ok = True
-
+    tests_to_run = []
     for lang_runner in glob("*/run*.sh"):
         lang = os.path.dirname(lang_runner)
         runner = os.path.basename(lang_runner)
@@ -53,32 +87,11 @@ def main() -> int:
             continue
         if args.default and sub != "release":
             continue
+        tests_to_run.append((lang, runner, sub, args.frames))
 
-        proc = subprocess.run(
-            [
-                f"./{runner}",
-                "--profile",
-                str(args.frames),
-                "--silent",
-                "--headless",
-                "--turbo",
-                TEST_ROM,
-            ],
-            cwd=lang,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            text=True,
-        )
-        frames = ""
-        for line in proc.stdout.split("\n"):
-            if "frames" in line:
-                frames = line
-        print(f"{lang:>5s} / {sub:7s}: {frames}")
-        if proc.returncode != 0:
-            all_ok = False
-            print(f"Output from failed run:\n{proc.stdout}")
-
-    return 0 if all_ok else 1
+    p = ThreadPool(8 if args.parallel else 1)
+    results = p.starmap(test, tests_to_run)
+    return 0 if all(results) else 1
 
 
 if __name__ == "__main__":
