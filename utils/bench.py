6b926120a119b8a932b1d751f6fc44060cb1243c
Shish <shish@shishnet.org>
2022-11-02 12:53:22 +0000

    [utils] have a github workflow to build docker image and run each implementation inside it, fixes #39

--- utils/bench.py
+++ utils/bench.py
@@ -12,34 +12,54 @@ import subprocess
 import os
 import re
 import sys
+import argparse
 
-for lang_runner in glob("*/run*.sh"):
-    lang = os.path.dirname(lang_runner)
-    runner = os.path.basename(lang_runner)
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--default", default=False, action="store_true", help="Only run the default run.sh, not variants")
+    parser.add_argument("--frames", type=int, default=600, help="Run for this many frames")
+    parser.add_argument("langs", default=[], nargs="*")
+    args = parser.parse_args()
 
-    if sys.argv[1:] and lang not in sys.argv[1:]:
-        continue
+    all_ok = True
 
-    sub = "release"
-    if match := re.match("run_(.*).sh", runner):
-        sub = match.group(1)
-    proc = subprocess.run(
-        [
-            f"./{runner}",
-            "--profile",
-            "600",
-            "--silent",
-            "--headless",
-            "--turbo",
-            "../test_roms/games/opus5.gb",
-        ],
-        cwd=lang,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        text=True,
-    )
-    frames = ""
-    for line in proc.stdout.split("\n"):
-        if "frames" in line:
-            frames = line
-    print(f"{lang:>5s} / {sub:7s}: {frames}")
+    for lang_runner in glob("*/run*.sh"):
+        lang = os.path.dirname(lang_runner)
+        runner = os.path.basename(lang_runner)
+        sub = "release"
+        if match := re.match("run_(.*).sh", runner):
+            sub = match.group(1)
+
+        if args.langs and lang not in args.langs:
+            continue
+        if args.default and sub != "release":
+            continue
+
+        proc = subprocess.run(
+            [
+                f"./{runner}",
+                "--profile",
+                str(args.frames),
+                "--silent",
+                "--headless",
+                "--turbo",
+                "../test_roms/games/opus5.gb",
+            ],
+            cwd=lang,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+        frames = ""
+        for line in proc.stdout.split("\n"):
+            if "frames" in line:
+                frames = line
+        print(f"{lang:>5s} / {sub:7s}: {frames}")
+        if proc.returncode != 0:
+            all_ok = False
+
+    return 0 if all_ok else 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- utils/shell.sh
+++ utils/shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
 docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t rosettaboy . && \
-docker run --rm --privileged -ti -v $(pwd)/..:/home/dev rosettaboy
+docker run --rm --privileged -ti -v $(pwd)/..:/home/dev rosettaboy $*
