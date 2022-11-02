a9f9347705b977f4de8ed3947300cd30518e2acc
Shish <shish@shishnet.org>
2022-11-02 12:59:14 +0000

    If we are running in a terminal, then run docker in terminal mode

--- utils/shell.sh
+++ utils/shell.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 cd $(dirname $0)
+
+# If we are running in a terminal, then run docker in terminal mode
+if [ -t 1 ] ; then
+    FLAGS=-ti
+else
+    FLAGS=
+fi
+
 docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t rosettaboy . && \
-docker run --rm --privileged -ti -v $(pwd)/..:/home/dev rosettaboy $*
+docker run --rm --privileged $FLAGS -v $(pwd)/..:/home/dev rosettaboy $*
