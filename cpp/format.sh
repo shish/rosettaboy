807efd1d545cd06dc3b0c66cca180947dfd9e4ec
Shish <shish@shishnet.org>
2022-11-03 13:24:50 +0000

    [cpp] support both clang-format and clang-format-14

--- cpp/format.sh
+++ cpp/format.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-clang-format-14 -i $(find src -type f | grep -v _args.h) --ferror-limit 10 -Werror
+if command -v clang-format-14 &> /dev/null ; then
+    CMD=clang-format-14
+else
+    CMD=clang-format
+fi
+$CMD -i $(find src -type f | grep -v _args.h) --ferror-limit 10 -Werror
