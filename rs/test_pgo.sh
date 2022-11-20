42326c30c53aa94a16845a68e8fbaf933bd84771
Shish <shish@shishnet.org>
2022-11-20 22:19:24 +0000

    vars

--- rs/test_pgo.sh
+++ rs/test_pgo.sh
@@ -1,23 +1,22 @@
 #!/bin/sh -eu
 TARGET=$(rustc -vV | grep host: | sed 's/host: //')
+PGO_DATA=/tmp/pgo-rs-data
 
 # STEP 0: Make sure there is no left-over profiling data from previous runs
-rm -rf /tmp/pgo-data
+rm -rf $PGO_DATA
 
 # STEP 1: Build the instrumented binaries
-RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" \
+RUSTFLAGS="-Cprofile-generate=$PGO_DATA" \
     cargo build --release --target=$TARGET
 
 # STEP 2: Run the instrumented binaries with some typical data
-./target/$TARGET/release/rosettaboy-rs --profile 600 --silent --headless --turbo ../test_roms/games/opus5.gb >/dev/null
-./target/$TARGET/release/rosettaboy-rs --profile 600 --silent --headless --turbo ../test_roms/games/opus5.gb >/dev/null
-./target/$TARGET/release/rosettaboy-rs --profile 600 --silent --headless --turbo ../test_roms/games/opus5.gb >/dev/null
+./target/$TARGET/release/rosettaboy-rs --frames 10 -SHt ../test_roms/games/opus5.gb >/dev/null
 
 # STEP 3: Merge the `.profraw` files into a `.profdata` file
-xcrun llvm-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
+llvm-profdata merge -o $PGO_DATA/merged.profdata $PGO_DATA
 
 # STEP 4: Use the `.profdata` file for guiding optimizations
-RUSTFLAGS="-Cprofile-use=/tmp/pgo-data/merged.profdata" \
+RUSTFLAGS="-Cprofile-use=$PGO_DATA/merged.profdata" \
     cargo build --release --target=$TARGET
 
 exec ./target/$TARGET/release/rosettaboy-rs $*
