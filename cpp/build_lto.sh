33410b12f5453af31e75031402045cfe7b4eb8d2
Shish <shish@shishnet.org>
2023-01-05 23:46:14 +0000

    [cpp] LTO build

---
+++ cpp/build_lto.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eu
+
+cd $(dirname $0)
+BUILDDIR=build/lto/$(uname)-$(uname -m)
+cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_LTO=On -B $BUILDDIR .
+cmake --build $BUILDDIR -j
+cp $BUILDDIR/rosettaboy-cpp ./rosettaboy-lto
