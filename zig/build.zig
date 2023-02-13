16f29a8eefc7b08d090a377ab449232351a209ca
Shish <shish@shishnet.org>
2023-02-13 18:41:11 +0000

    zig 0.11

--- zig/build.zig
+++ zig/build.zig
@@ -7,20 +7,22 @@ pub fn build(b: *std.build.Builder) void {
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-    const sdk = Sdk.init(b);
+    const sdk = Sdk.init(b, null);
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("rosettaboy", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const exe = b.addExecutable(.{
+        .name = "rosettaboy",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     sdk.link(exe, .dynamic);
-    exe.addPackage(sdk.getWrapperPackage("sdl2"));
+    exe.addModule("sdl2", sdk.getWrapperModule());
     exe.install();
-    exe.addLibraryPath("/Users/shish2k/homebrew/lib/");
-    exe.addPackagePath("clap", "lib/clap/clap.zig");
+    exe.addAnonymousModule("clap", .{ .source_file = .{ .path = "lib/clap/clap.zig" } });
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -31,9 +33,11 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
