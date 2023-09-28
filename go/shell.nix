30928b9446a9a7a85b80fb0e9da524205f7d1c93
Shish <shish@shishnet.org>
2023-09-29 00:49:50 +0100

    [all] remove nix
    When this works, it's slow; when it doesn't work, I can't figure out how to fix it ;(

--- go/shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} } : pkgs.mkShell {
-	buildInputs = with pkgs; [
-		go
-		SDL2
-		
-		pkg-config
-	];
-}
--- utils/shell.sh
+++ utils/shell.sh
@@ -4,18 +4,4 @@ set -eu
 cd $(dirname $0)
 cd ..
 
-if nix develop --help 2>/dev/null 1>/dev/null; then
-    if [ -n "$*" ]; then
-        nix develop . --command bash -c "$*"
-    else
-        nix develop .
-    fi
-elif command -v nix-shell; then
-    if [ -n "$*" ]; then
-        nix-shell --run "$*"
-    else
-        nix-shell
-    fi
-else
-    ./utils/shell-docker "$@"
-fi
+./utils/shell-docker "$@"
