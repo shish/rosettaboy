5ec102a7d6bf6bca011cfdeb47c62c997ffceada
Shish <shish@shishnet.org>
2024-04-06 11:11:28 +0100

    [c] make sure fread() return is checked

--- c/src/ram.c
+++ c/src/ram.c
@@ -54,7 +54,8 @@ void ram_ctor(struct RAM *self, struct Cart *cart, bool debug) {
     // Load a real bootloader if available
     FILE *fp = fopen("boot.gb", "rb");
     if(fp) {
-        fread(&BOOT, 1, 0x100, fp);
+        int bytes_read = fread(&BOOT, 1, 0x100, fp);
+        assert(bytes_read == 0x100);
         fclose(fp);
 
         // NOP the DRM
