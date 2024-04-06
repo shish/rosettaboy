fe9f81c84275ffced3d6aa2786ce6a04848b39cb
Shish <shish@shishnet.org>
2024-04-06 21:34:19 +0100

    [many] BUILD_ROOT environment var to minimise pollution in the source repo

--- rs/build.sh
+++ rs/build.sh
@@ -2,5 +2,7 @@
 set -eu
 
 cd $(dirname $0)
+BUILDDIR=${BUILD_ROOT:-target}/$(basename $(pwd))-$(echo $(basename $0) | sed 's/build_*\(.*\).sh/\1/')-$(uname)-$(uname -m)
+export CARGO_TARGET_DIR=$BUILDDIR
 cargo build --release
-cp ./target/release/rosettaboy-rs ./rosettaboy-release
+cp $BUILDDIR/release/rosettaboy-rs ./rosettaboy-release
