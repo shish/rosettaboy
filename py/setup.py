d14fa19f6886d0e7cc03e5f207591c692f6dd6bc
Shish <shish@shishnet.org>
2024-04-06 12:16:26 +0100

    [py] cleanups

--- py/setup.py
+++ py/setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup
 import os
 
-USE_MYPYC = False
 if os.getenv('ROSETTABOY_USE_MYPYC', None) == '1':
-    USE_MYPYC = True
-
-if USE_MYPYC:
     from mypyc.build import mypycify
     packages = []
     ext_modules=mypycify(
