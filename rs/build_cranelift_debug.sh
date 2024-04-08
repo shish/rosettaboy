e8e6e9b63eb6d4588938c5a0c319d4e848880d75
Shish <shish@shishnet.org>
2024-04-08 13:40:36 +0100

    [rs] BUILD_ROOT environment var to minimise pollution in the source repo

--- rs/build_cranelift_debug.sh
+++ rs/build_cranelift_debug.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)
 BUILDDIR=${BUILD_ROOT:-target}/$(basename $(pwd))-cranelift-$(echo $(basename $0) | sed 's/build_*\(.*\).sh/\1/')-$(uname)-$(uname -m)
 export CARGO_PROFILE_DEV_CODEGEN_BACKEND=cranelift
 export CARGO_TARGET_DIR=$BUILDDIR
-cargo +nightly build -Zcodegen-backend --release
+cargo +nightly build -Zcodegen-backend
 cp $BUILDDIR/debug/rosettaboy-rs ./rosettaboy-cranelift
