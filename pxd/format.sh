d3901fdfa8cb2508d5b53d9d6c149177f2835dfc
Shish <shish@shishnet.org>
2024-04-08 13:56:19 +0100

    [pxd] BUILD_ROOT environment var to minimise pollution in the source repo

--- pxd/format.sh
+++ pxd/format.sh
@@ -3,6 +3,10 @@ set -eu
 
 cd $(dirname $0)
 
-source py_env.sh
+VENVDIR=${BUILD_ROOT:-$(realpath $(dirname $0))/build}/$(basename $(pwd))-$(uname)-$(uname -m)-black
+if [ ! -d $VENVDIR ]; then
+	python3 -m venv $VENVDIR
+	$VENVDIR/bin/pip install pysdl2 pysdl2-dll mypy black Cython==3.0.0a11
+fi
 
-exec black src/*.py
+$VENVDIR/bin/black src/*.py
