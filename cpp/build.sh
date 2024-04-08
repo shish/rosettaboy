ff8aa45b23f356c81fd8770d16ac1544eb974eae
Shish <shish@shishnet.org>
2024-04-08 13:40:44 +0100

    [cpp] BUILD_ROOT environment var to minimise pollution in the source repo

--- cpp/build.sh
+++ cpp/build.sh
@@ -2,7 +2,7 @@
 set -eu
 
 cd $(dirname $0)
-BUILDDIR=build/release/$(uname)-$(uname -m)
+BUILDDIR=${BUILD_ROOT:-build}/$(basename $(pwd))-$(echo $(basename $0) | sed 's/build_*\(.*\).sh/\1/')-$(uname)-$(uname -m)
 cmake -DCMAKE_BUILD_TYPE=Release -B $BUILDDIR .
 cmake --build $BUILDDIR -j
 cp $BUILDDIR/rosettaboy-cpp ./rosettaboy-release
