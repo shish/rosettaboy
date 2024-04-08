fcaeb40e2fe0987bb94dae75bd00acb4a60d6acb
Shish <shish@shishnet.org>
2024-04-08 13:58:22 +0100

    [py] BUILD_ROOT environment var to minimise pollution in the source repo

--- py/build.sh
+++ py/build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 set -eu
 cd $(dirname $0)
-source py_env.sh
+
+BUILDDIR=${BUILD_ROOT:-$(realpath $(dirname $0))/build}/$(basename $(pwd))-$(echo $(basename $0) | sed 's/build_*\(.*\).sh/\1/')-$(uname)-$(uname -m)-venv
+if [ ! -d $BUILDDIR ] ; then
+	python3 -m venv $BUILDDIR
+	$BUILDDIR/bin/pip install pysdl2 pysdl2-dll
+fi
+source $BUILDDIR/bin/activate
 
 cat >rosettaboy-release <<EOD
 #!/usr/bin/env bash
 set -eu
-source "\$(dirname \$0)/py_env.sh"
+source $BUILDDIR/bin/activate
 PYTHONPATH="\$(dirname \$0)" exec python3 -c "from src.main import main ; import sys ; sys.exit(main(sys.argv))" \$*
 EOD
 chmod +x rosettaboy-release
