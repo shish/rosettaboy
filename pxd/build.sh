390c6e74ac29232d9b250afcf9b4ac84494b6956
Shish <shish@shishnet.org>
2024-04-09 01:07:32 +0100

    [py] bump to 3.12

--- pxd/build.sh
+++ pxd/build.sh
@@ -8,7 +8,7 @@ VENVDIR=${BUILD_ROOT:-$(realpath $(dirname $0))/build}/$(basename $(pwd))-$(echo
 
 if [ ! -d $VENVDIR ]; then
 	python3 -m venv $VENVDIR
-	$VENVDIR/bin/pip install pysdl2 pysdl2-dll mypy black Cython==3.0.0a11
+	$VENVDIR/bin/pip install pysdl2 pysdl2-dll Cython setuptools
 fi
 source $VENVDIR/bin/activate
 
--- py/build_mypyc.sh
+++ py/build_mypyc.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 BUILDDIR=${BUILD_ROOT:-$(realpath $(dirname $0))/build}/$(basename $(pwd))-$(echo $(basename $0) | sed 's/build_*\(.*\).sh/\1/')-$(uname)-$(uname -m)-venv
 if [ ! -d $BUILDDIR ] ; then
 	python3 -m venv $BUILDDIR
-	$BUILDDIR/bin/pip install pysdl2 pysdl2-dll 'mypy>=1.0.0'
+	$BUILDDIR/bin/pip install pysdl2 pysdl2-dll 'mypy[mypyc]' setuptools
 fi
 source $BUILDDIR/bin/activate
 
