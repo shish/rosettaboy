87b14202ba8fe97c0fcfc4bd8d9f61c5b40c1266
Shish <shish@shishnet.org>
2024-05-08 03:49:48 +0100

    [py] add pypy

---
+++ py/build_pypy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu
+cd $(dirname $0)
+
+BUILDDIR=${BUILD_ROOT:-$(realpath $(dirname $0))/build}/$(basename $(pwd))-$(echo $(basename $0) | sed 's/build_*\(.*\).sh/\1/')-$(uname)-$(uname -m)-venv
+if [ ! -d $BUILDDIR ] ; then
+	~/.pypy/bin/pypy3 -m venv $BUILDDIR
+	$BUILDDIR/bin/pip install pysdl2 pysdl2-dll
+fi
+source $BUILDDIR/bin/activate
+
+cat >rosettaboy-pypy <<EOD
+#!/usr/bin/env bash
+set -eu
+source $BUILDDIR/bin/activate
+exec python3 -c "from src.main import main ; import sys ; sys.exit(main(sys.argv))" \$*
+EOD
+chmod +x rosettaboy-pypy
