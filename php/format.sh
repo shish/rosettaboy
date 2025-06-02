c8024c64bcd440317b2907d3ae961aea80543fdc
Shish <shish@shishnet.org>
2025-06-02 16:03:03 +0100

    [php] version bumps

--- php/format.sh
+++ php/format.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)
 
 composer install
 PHP_CS_FIXER_IGNORE_ENV=1 ./vendor/bin/php-cs-fixer fix
-./vendor/bin/phpstan analyse --error-format raw -c .phpstan.neon
+./vendor/bin/phpstan analyse --error-format raw
