2a2663ca8d5b67ede29443e7b20fdaa59cc665c3
Shish <shish@shishnet.org>
2025-07-01 13:46:22 +0100

    [rs] remove flaky cranelift build

--- rs/build_cranelift_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-cd $(dirname $0)
-BUILDDIR=${BUILD_ROOT:-target}/$(basename $(pwd))-cranelift-$(echo $(basename $0) | sed 's/build_*\(.*\).sh/\1/')-$(uname)-$(uname -m)
-export CARGO_PROFILE_DEV_CODEGEN_BACKEND=cranelift
-export CARGO_TARGET_DIR=$BUILDDIR
-cargo +nightly build -Zcodegen-backend
-cp $BUILDDIR/debug/rosettaboy-rs ./rosettaboy-cranelift
