24e58d82d2d3b77ea06370214d6d97316592fe1d
Shish <shish@shishnet.org>
2025-07-01 13:44:47 +0100

    fix shell.sh

--- utils/shell.sh
+++ utils/shell.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -eu
-
 cd $(dirname $0)
-cd ..
 
-./utils/shell-docker "$@"
+# If we are running in a terminal, then run docker in terminal mode
+if [ -t 1 ] ; then
+    FLAGS=-ti
+else
+    FLAGS=
+fi
+
+docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t rosettaboy .
+docker run --rm --privileged $FLAGS -v $(pwd)/..:/home/dev/rosettaboy rosettaboy $*
